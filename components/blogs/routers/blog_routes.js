4464c24d969c36da1d99bfc5c87b93834634d07c
mamta1138 <ghimire.mamta98@gmail.com>
2025-04-18 15:03:53 +0545

    added a controller to fetch blog by slug and made changes to routes.

--- components/blogs/routers/blog_routes.js
+++ components/blogs/routers/blog_routes.js
@@ -14,6 +14,7 @@ const getSingleBlog = require("../controllers/get_single_blog");
 const updateBlog = require("../controllers/update_blog");
 const deleteBlog = require("../controllers/delete_blog");
 const verifyBlog = require("../controllers/verify_blog");
+const getBlogBySlug = require("../controllers/get_blog_by_slug");
 
 router.post("/", verifyToken, checkRole("admin", "editor"), upload.fields([{ name: "image", maxCount: 10 }]), createBlog);
 router.get("/", listAllBlogs);
@@ -21,5 +22,6 @@ router.get("/:id", getSingleBlog);
 router.put("/:id", verifyToken, checkRole("admin", "editor"), updateBlog);
 router.delete("/:id", verifyToken, checkRole("admin"), deleteBlog);
 router.put("/verify/:id", verifyToken, checkRole("admin"), verifyBlog);
+router.get("/slug/:slug", getBlogBySlug);
 
 module.exports = router;
