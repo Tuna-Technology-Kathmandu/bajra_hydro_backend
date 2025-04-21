33099078cd26de2157c7e28cc1c5f8f87474195a
AbishekKhanal <abishekkhanal2056@gmail.com>
2025-04-21 12:20:38 +0545

    Refactor job model and validation: update job schema to vacancy schema, add new fields for category, location, job type, salary, requirements, benefits, application deadline, and is_active; enhance job creation logic with slug generation

--- components/jobs_available/controllers/create_job.js
+++ components/jobs_available/controllers/create_job.js
@@ -1,16 +1,19 @@
-const Job = require("../models/job_model");  
-const jobValidation = require("../helper/job_validator"); 
+const Job = require("../models/job_model");
+const slugify = require("slugify");
+const jobValidation = require("../helper/job_validator");
 
 const createJob = async (req, res) => {
   try {
-
     const { error, value } = jobValidation.validate(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
     }
 
+    const slug = slugify(value.title, { lower: true });
+
     const newJob = new Job({
-      ...value,  
+      ...value,
+      slug,
     });
 
     await newJob.save();
@@ -19,9 +22,7 @@ const createJob = async (req, res) => {
       message: "Job created successfully",
       newJob,
     });
-
   } catch (error) {
-
     console.error("Create Job Error:", error);
     return res.status(500).json({ message: "Server error while creating job" });
   }
